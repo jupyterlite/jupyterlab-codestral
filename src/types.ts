b2e46dbaefe17fbb81f4778fc5307f27b4b507e3
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-12-10 09:30:59 +0100

    Bump  package

--- src/types.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Fix a build error in langchainjs.
- * See https://github.com/langchain-ai/langchainjs/issues/7332
- */
-declare module '@anthropic-ai/sdk/resources/index.mjs' {
-  export type Tool = any;
-}
